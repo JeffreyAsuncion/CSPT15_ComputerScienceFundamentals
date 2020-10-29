e837e7b4020e7a0dea45fa9a767a78209bd1cc16
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-29 14:02:33 -0400

    Java Problem03 still working on case switching

--- Module01_JavaBasics/Problem03.java
+++ Module01_JavaBasics/Problem03.java
@@ -6,7 +6,7 @@ public class Problem03 {
     
 	    
 	// Approach1
-	/*
+	
         String str = "Welcome To Java"; // Original string
         // Step 1 - split string into words
 	String[] words = str.split(" ");  //Splitting into words
@@ -20,14 +20,24 @@ public class Problem03 {
 	    // start from end of word	
 	    for (int i=w.length()-1; i >=0; i--) // Welcome
 	    {
-	        reverseword = reverseword + w.charAt(i);
+	        /* THIS DOES NOT WORK AS PLANNED
+		if (Character.isUpperCase(w.charAt(i)))
+		{
+			w.charAt(i) = w.toLowerCase();
+		}
+		else
+		{
+			w.charAt(i) = w.charAt(i).toUppercase();
+		}
+		*/
+		reverseword = reverseword + w.charAt(i);
 	    }
 	    // Step 3 - rebuild the String with reverseword from 2a
 	    reverseString = reverseString + reverseword + " "; // emocleW
 	}
 	System.out.println(reverseString);
-	*/
 	
+	/*	
 	// Approach2
 	
         String str = "Welcome To Java"; // Original string
@@ -48,7 +58,7 @@ public class Problem03 {
 	}
 
 	System.out.println(reverseword);
-	
+	*/
 	
     }
 }
